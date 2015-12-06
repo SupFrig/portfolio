9cc33d04d67dcf40478607fc93f4e4de3c78fe41
SupFrig <montet.thomas@gmail.com>
2015-12-07 00:14:57 +0100

    maj

--- index.php
+++ index.php
@@ -7,124 +7,135 @@
 <?php $is_dev ? include('inc/head_dev.php') : include('inc/head.php'); ?>
 <body>
     <header>
-		<div class="logo">
-			<h1>Thomas Montet</h1>
-			<strong>Le mec qu'a pas d'idées</strong>
-		</div><!--
-			--><nav class="menu">
+		<div class="valign">
+			<div class="logo">
+				<h1>Thomas Montet</h1>
+				<strong>Le mec qu'a pas d'idées</strong>
+			</div><!--
+				--><nav class="menu">
+				<ul>
+					<li><a href="#">Porfolio</a></li>
+					<li><a href="#">About me</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<nav class="filters">
 			<ul>
-				<li><a href="#">Porfolio</a></li>
-				<li><a href="#">About me</a></li>
-				<li><a href="#">Contact</a></li>
+				<li><a href="#" data-filter=".insect">Insectes</a></li>
+				<li><a href="#" data-filter=".eye">Zyeux</a></li>
+				<li><a href="#" data-filter=".baby">Baby</a></li>
+				<li><a href="#" data-filter=".random">Random</a></li>
 			</ul>
 		</nav>
 	</header>
 	<section class="projects">
 		<ul>
-			<li class="grid-sizer">
-			<li class="item">
+			<li class="grid-sizer"></li>
+			<li class="item eye">
+				<a href="#">
+					<img src="img/data/dancing_alien.jpg"/>
+				</a>
+			</li><!--
+			--><li class="item eye random">
 				<a href="#">
 					<div class="mask">
 						<div class="inner">
 							<hr/>
 						</div>
 					</div>
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/psyche.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item random">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/rawr.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item random">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/realbaby.png"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/400x565/ff0000/dbdbdb"/>
+					<img src="img/data/1.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/2.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/3.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/4.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item insect">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/scarab.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item eye">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/troizyeux.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item eye">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/warp.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/5.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/6.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/7.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/8.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/9.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/10.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
+					<img src="img/data/11.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/13.jpg"/>
 				</a>
 			</li><!--
-			--><li class="item">
+			--><li class="item baby">
 				<a href="#">
-					<img src="http://dummyimage.com/565x400/000000/dbdbdb"/>
-				</a>
-			</li><!--
-			--><li class="item">
-				<a href="#">
-					<img src="http://dummyimage.com/565x400/ff0000/dbdbdb"/>
+					<img src="img/data/14.jpg"/>
 				</a>
 			</li>
 		</ul>
--- js/dev/6_base.js
+++ js/dev/6_base.js
@@ -1,11 +1,11 @@
 var Tm = {};
 
 jQuery(document).ready(function(){
-	Tm.grid();
+	Tm.isotope();
 });
 
 
-Tm.grid = function(){
+Tm.isotope = function(){
 	var grid = jQuery('.projects ul');
 	
 	grid.isotope({
@@ -17,6 +17,44 @@ Tm.grid = function(){
 	});
 	
 	grid.imagesLoaded( function() {
+		
+		//size grid items
+		grid.find('li').each(function(){
+			var $this = jQuery(this);
+			var img = $this.find('img');
+			if(img.length){
+				var ratio = img.width() / img.height();
+
+				if(ratio < 1){
+					console.log('vertical');
+					var height = img.width() * (ratio);
+					console.log(height);
+				}else{
+					
+				}
+			}
+		});
+		
 		grid.isotope('layout');
 	});
+	
+	
+	
+	//filters function
+	jQuery('.filters').on('click','a',function(){
+		var $this = jQuery(this);
+		var generalFilter = '';
+		$this.toggleClass('active');
+		jQuery('.filters a.active').each(function(){
+			generalFilter += jQuery(this).attr('data-filter')+',';
+		});
+		generalFilter = generalFilter.slice(0, -1);
+		
+		grid.isotope({filter: generalFilter});
+		return false;
+	});
+}
+
+Tm.popin = function(){
+	
 }
